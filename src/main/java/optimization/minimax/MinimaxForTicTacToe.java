94d80390eac42f14069f8a1d356b6dc94d3ec435
Brian Chen <brianchen0810@hotmail.com>
2020-08-23 03:02:52 -0400

    Add Minimax with alpha beta pruning

--- src/main/java/optimization/minimax/MinimaxForTicTacToe.java
+++ src/main/java/optimization/minimax/MinimaxForTicTacToe.java
@@ -49,6 +49,58 @@ public class MinimaxForTicTacToe {
         }
     }
 
+    public static MinimaxForTicTacToeResult minimaxWithAlphaBetaPruning(char[][] position, int depth, int alpha, int beta, boolean maximizingPlayer) {
+        if (depth == 0 || TicTacToe.isGameOver(position)) {
+            return new MinimaxForTicTacToeResult(null, evaluatePosition(position));
+        }
+
+        if (maximizingPlayer) {
+            int maxEvaluation = Integer.MIN_VALUE;
+            Move bestMove = new Move();
+
+            List<Move> moves = TicTacToe.getAllEmptySpace(position);
+
+            for (Move move : moves) {
+                position[move.getRow()][move.getColumn()] = Player.COMPUTER.getToken();
+                MinimaxForTicTacToeResult result = minimaxWithAlphaBetaPruning(position, depth - 1, alpha, beta, false);
+                position[move.getRow()][move.getColumn()] = ' ';
+                if (result.getEvaluation() > maxEvaluation) {
+                    maxEvaluation = result.getEvaluation();
+                    bestMove = move;
+
+                    alpha = Math.max(alpha, maxEvaluation);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+
+            return new MinimaxForTicTacToeResult(bestMove, maxEvaluation);
+        } else {
+            int minEvaluation = Integer.MAX_VALUE;
+            Move worstMove = new Move();
+
+            List<Move> moves = TicTacToe.getAllEmptySpace(position);
+
+            for (Move move : moves) {
+                position[move.getRow()][move.getColumn()] = Player.HUMAN.getToken();
+                MinimaxForTicTacToeResult result = minimaxWithAlphaBetaPruning(position, depth - 1, alpha, beta, true);
+                position[move.getRow()][move.getColumn()] = ' ';
+                if (result.getEvaluation() < minEvaluation) {
+                    minEvaluation = result.getEvaluation();
+                    worstMove = move;
+
+                    beta = Math.min(beta, minEvaluation);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+
+            return new MinimaxForTicTacToeResult(worstMove, minEvaluation);
+        }
+    }
+
     private static int evaluatePosition(char[][] position) {
         if (TicTacToe.checkWinner(position) == Player.COMPUTER) {
             return 1;
